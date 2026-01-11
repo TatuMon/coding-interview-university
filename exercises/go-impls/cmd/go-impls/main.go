8f531875f091f75665fcfc0e7cbe312875427933
tatumon <tatumonar@proton.me>
2026-01-11 20:58:33 -0300

    Last methods of the Golang vector implementation

--- exercises/go-impls/cmd/go-impls/main.go
+++ exercises/go-impls/cmd/go-impls/main.go
@@ -21,4 +21,18 @@ func main() {
 
 	myVector.Insert(77, 15)
 	fmt.Printf("After inserting 77 into index 15: %s\n", myVector.SPrint())
+
+	myVector.Pop()
+	fmt.Printf("After popping: %s\n", myVector.SPrint())
+
+	myVector.Delete(2)
+	fmt.Printf("After deleting value at index 2: %s\n", myVector.SPrint())
+
+	myVector.Set(88, 9)
+	myVector.Set(88, 11)
+	myVector.Set(88, 13)
+	fmt.Printf("After setting 88 in multiple indices: %s\n", myVector.SPrint())
+
+	myVector.Remove(88)
+	fmt.Printf("After removing all 88 instances: %s\n", myVector.SPrint())
 }
--- exercises/go-impls/pkg/vector/vector.go
+++ exercises/go-impls/pkg/vector/vector.go
@@ -5,11 +5,11 @@ import (
 	"strings"
 )
 
-type Vector[T any] struct {
+type Vector[T comparable] struct {
 	arr []T // Length and capacity is already handled by the slice type
 }
 
-func New[T any](capacity uint) Vector[T] {
+func New[T comparable](capacity uint) Vector[T] {
 	return Vector[T]{
 		arr: make([]T, 0, capacity),
 	}
@@ -26,7 +26,26 @@ func (v *Vector[T]) SPrint() string {
 	}
 	fmt.Fprint(&b, "]")
 
+	fmt.Fprintf(&b, " len=%d", len(v.arr))
+	fmt.Fprintf(&b, " cap=%d", cap(v.arr))
+
 	return b.String()
+}
+
+func (v *Vector[T]) Len() uint {
+	return uint(len(v.arr))
+}
+
+func (v *Vector[T]) Cap() uint {
+	return uint(cap(v.arr))
+}
+
+func (v *Vector[T]) At(index uint) T {
+	return v.arr[index]
+}
+
+func (v *Vector[T]) Set(value T, index uint) {
+	v.arr[index] = value
 }
 
 func (v *Vector[T]) Push(values ...T) {
@@ -52,4 +71,42 @@ func (v *Vector[T]) Insert(value T, index uint) {
 	newRightSide := append([]T{value, replacedVal}, v.arr[index+1:]...)
 
 	v.arr = append(leftSide, newRightSide...)
+}
+
+func (v *Vector[T]) Prepend(value T) {
+	v.Insert(value, 0)
+}
+
+func (v *Vector[T]) Pop() T {
+	popped := v.arr[len(v.arr)-1]
+	v.arr = v.arr[:len(v.arr)-1]
+
+	return popped
+}
+
+func (v *Vector[T]) Delete(index uint) {
+	if index >= uint(len(v.arr)) {
+		return
+	}
+
+	rightSide := v.arr[index+1:]
+	copy(v.arr[index:], rightSide)
+}
+
+func (v *Vector[T]) Remove(value T) {
+	for i, j := range v.arr {
+		if j == value {
+			v.Delete(uint(i))
+		}
+	}
+}
+
+func (v *Vector[T]) First(value T) int {
+	for i, j := range v.arr {
+		if j == value {
+			return i
+		}
+	}
+
+	return -1
 }
